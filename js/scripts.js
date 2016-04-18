14204caea162e1bc2fff1fdef0e88da7db689303
brandonlee781 <brandonlee781@gmail.com>
2016-04-18 02:12:07 -0500

    minor changes and adjusting of layout

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//open and close nav drawer
 $(".menu-button").click(function(){
   $("nav").toggleClass("open-drawer");
   $(".wrapper").toggleClass("move-wrap");
@@ -5,4 +6,35 @@ $(".menu-button").click(function(){
 $("nav a").click(function(){
   $("nav").toggleClass("open-drawer");
   $(".wrapper").toggleClass("move-wrap");
+});
+//rotating skills images
+$(window).load(function() { //start after HTML, images have loaded
+    var InfiniteRotator = {
+        init: function(){
+            //initial fade-in time (in milliseconds)
+            var initialFadeIn = 500;
+            //interval between items (in milliseconds)
+            var itemInterval = 2000;
+            //cross-fade time (in milliseconds)
+            var fadeTime = 1500;
+            //count number of items
+            var numberOfItems = $('.rotating-item').length;
+            //set current item
+            var currentItem = 0;
+            //show first item
+            $('.rotating-item').eq(currentItem).fadeIn(initialFadeIn);
+            //loop through the items
+            var infiniteLoop = setInterval(function(){
+                $('.rotating-item').eq(currentItem).fadeOut(fadeTime);
+                if(currentItem == numberOfItems -1){
+                    currentItem = 0;
+                }else{
+                    currentItem++;
+                }
+                $('.rotating-item').eq(currentItem).fadeIn(fadeTime);
+            }, itemInterval);
+        }
+    };
+    InfiniteRotator.init();
+ 
 });
